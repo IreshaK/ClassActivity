50a09d1997d40efcf6a09cd778db4a772272e7ed
IreshaK <ireshakthilakarathne@gmail.com>
2023-11-09 15:16:06 +1100

    just added

--- hello.js
+++ hello.js
@@ -1,3 +1,17 @@
 console.log("Hello World..!");
 
 console.log("Hello form local");
+
+/*
+
+git clone <remote -httpp://>
+
+git add filename.extention - add file
+git commit -m "message" - commit a file
+
+git status
+git log
+
+git push origin - to send your file to origin/remote
+
+*/
